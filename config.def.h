d3dd221b87e7c790e529d849b6767c6c9706b113
James Monk <james@jamesmonk.me>
2021-01-20 15:54:59 +0100

    Update of gitignore to ignore config.h as built file, using git so merging in upstream updates is all gravy, and we want to be minimal here right?!
    Custom config.def.h honed, working well for everything applied via
    patches including all my custom bits.

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static unsigned int borderpx        = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 16;        /* gaps between windows */
+static unsigned int snap            = 32;       /* snap pixel */
+static int showbar                  = 1;        /* 0 means no bar */
+static int topbar                   = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
@@ -28,9 +28,17 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title           tags mask     isfloating   monitor */
+	{ "Zathura",  NULL,       NULL,           0,            1,           -1 },
+	{ "Sxiv",     NULL,       NULL,           0,            1,           -1 },
+	{ "st",       NULL,       "calculator",   0,            1,           -1 },
+	{ "st",       NULL,       "newsboat",     0,            1,           -1 },
+	{ "st",       NULL,       "neomutt",      0,            1,           -1 },
+	{ "st",       NULL,       "calcurse",     0,            1,           -1 },
+	{ "st",       NULL,       "ncmpcpp",      0,            1,           -1 },
+	{ "st",       NULL,       "sudo",         0,            1,           -1 },
+	{ "st",       NULL,       "less",         0,            1,           -1 },
+	{ "mpv",      NULL,       NULL,           0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -46,7 +54,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -82,45 +90,56 @@ ResourcePref resources[] = {
 		{ "mfact",      	 	FLOAT,   &mfact },
 };
 
+#include <X11/XF86keysym.h>
+
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier                     key                        function        argument */
+	{ MODKEY,                       XK_d,                      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,                 spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,                  togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY|ShiftMask,             XK_b,                      togglebar,      {0} },
+	{ MODKEY,                       XK_l,                      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_h,                      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,                      incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,                      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_h,                      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_l,                      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_Return,                 zoom,           {0} },
+	{ MODKEY,                       XK_Tab,                    view,           {0} },
+	{ MODKEY,                       XK_q,                      killclient,     {0} },
+	{ MODKEY,                       XK_t,                      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,                      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,                      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,                  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,                  togglefloating, {0} },
+	{ MODKEY,                       XK_0,                      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                      tag,            {.ui = ~0 } },
+	{ MODKEY|ShiftMask,     	XK_Print,   	           spawn,          SHCMD("scrot -s '%Y-%m-%d_$wx$h.png' -e 'mv $f ~/Pictures/'") },
+	{ MODKEY,		        XK_Print,   	           spawn,          SHCMD("scrot '%Y-%m-%d_$wx$h.png' -e 'mv $f ~/Pictures/'") },
+	{ 0,			        XF86XK_AudioMute,	   spawn,          SHCMD("pamixer -t; kill -39 $(pidof dwmblocks)") },
+	{ 0,			        XF86XK_AudioRaiseVolume,   spawn,          SHCMD("pamixer --allow-boost -i 1; kill -39 $(pidof dwmblocks)") },
+	{ 0,			        XF86XK_AudioLowerVolume,   spawn,          SHCMD("pamixer --allow-boost -d 1; kill -39 $(pidof dwmblocks)") },
+	{ 0,			        XF86XK_AudioPrev,	   spawn,          SHCMD("mpc prev; kill -35 $(pidof dwmblocks)") },
+	{ 0,			        XF86XK_AudioNext,	   spawn,          SHCMD("mpc next; kill -35 $(pidof dwmblocks)") },
+	{ 0,			        XF86XK_AudioPlay,	   spawn,          SHCMD("mpc toggle; kill -35 $(pidof dwmblocks)") },
+	{ 0,			        XF86XK_AudioStop,	   spawn,          SHCMD("mpc stop; kill -35 $(pidof dwmblocks)") },
+	{ 0,                            XF86XK_MonBrightnessUp,    spawn,          SHCMD("xbacklight -inc 10") },
+	{ 0,                            XF86XK_MonBrightnessDown,  spawn,          SHCMD("xbacklight -dec 10") },
+	{ 0,                            XK_F1,                     spawn,          SHCMD("firefox") },
+	{ 0,                            XK_F2,                     spawn,          SHCMD("st -g 175x50 -e neomutt") },
+	{ 0,                            XK_F3,                     spawn,          SHCMD("st -g 175x50 -e newsboat") },
+	{ 0,                            XK_F4,                     spawn,          SHCMD("st -t calculator -g 65x20 -e calc") },
+	{ 0,                            XK_F5,                     spawn,          SHCMD("st -g 175x50 -e ncmpcpp") },
+	TAGKEYS(                        XK_1,                                      0)
+	TAGKEYS(                        XK_2,                                      1)
+	TAGKEYS(                        XK_3,                                      2)
+	TAGKEYS(                        XK_4,                                      3)
+	TAGKEYS(                        XK_5,                                      4)
+	TAGKEYS(                        XK_6,                                      5)
+	TAGKEYS(                        XK_7,                                      6)
+	TAGKEYS(                        XK_8,                                      7)
+	TAGKEYS(                        XK_9,                                      8)
+	{ MODKEY|ShiftMask,             XK_q,                      quit,           {0} },
 };
 
 /* button definitions */
@@ -133,6 +152,8 @@ static Button buttons[] = {
 	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
 	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
 	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
+	{ ClkStatusText,        0,              Button4,        sigdwmblocks,   {.i = 4} },
+	{ ClkStatusText,        0,              Button5,        sigdwmblocks,   {.i = 5} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
